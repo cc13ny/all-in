1ba397ffbe5b2bc0b5362c2680210f44f125fdf6
ChenCBupt <chenchiapply@gmail.com>
2014-07-08 12:48:31 -0400

    Create TreeLinkNode.java

---
+++ leetcode/java/PopulatingNextRightPointersinEachNode/TreeLinkNode.java
@@ -0,0 +1,13 @@
+/**
+ * Definition for binary tree with next pointer.
+ */
+
+ public class TreeLinkNode {
+	 int val;
+	 
+	 TreeLinkNode left, right, next;
+	 
+	 TreeLinkNode(int x){ 
+		 val = x; 
+	 }
+ }
