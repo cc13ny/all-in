e60db9d7c162d6a99c40a4e5225eda9e4e031dc1
cyc <chenchiapply@gmail.com>
2015-07-11 17:38:55 -0400

    Create TwoSum_002.java

---
+++ leetcode/TwoSum/TwoSum_002.java
@@ -0,0 +1,19 @@
+import java.util.Hashtable;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Hashtable<Integer, Integer> dict = new Hashtable<Integer, Integer>();
+        int len = nums.length;
+        int[] res = new int[2];
+        for(int i=0; i<len; i++){
+            if(dict.containsKey(target - nums[i])){
+                res[0] = dict.get(target - nums[i])+1;
+                res[1] = i+1;
+                return res;
+            }
+            dict.put(nums[i], i);
+        }
+        
+        return res;
+    }
+}
