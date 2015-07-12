857890c435da150fdca4ce77491223b42e9bd18e
cyc <chenchiapply@gmail.com>
2015-07-12 02:26:33 -0400

    Create LongestCommonPrefix_002.py

---
+++ leetcode/LongestCommonPrefix/py/LongestCommonPrefix_002.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param {string[]} strs
+    # @return {string}
+    def longestCommonPrefix(self, strs):
+        res = ''
+        if strs == []:
+            return res
+            
+        lens = [len(s) for s in strs]
+        mi = min(lens)
+        for i in range(mi):
+            a = set([s[i] for s in strs])
+            if len(a) != 1:
+                break
+            else:
+                res+=a.pop()
+        return res
