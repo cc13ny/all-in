afec9ce6144e83413cb88c6e892c858a08923475
cyc <chenchiapply@gmail.com>
2015-07-12 11:34:04 -0400

    Create AddBinary_001.py

---
+++ leetcode/AddBinary/py/AddBinary_001.py
@@ -0,0 +1,7 @@
+class Solution:
+    # @param {string} a a number
+    # @param {string} b a number
+    # @return {string} the result
+    def addBinary(self, a, b):
+        # Write your code here
+        return bin(int(a, 2) + int(b, 2))[2:]
