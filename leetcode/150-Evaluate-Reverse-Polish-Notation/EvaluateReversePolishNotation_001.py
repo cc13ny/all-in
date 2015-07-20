401ae291f2981929b5cd1399700ece543563a6f6
cyc <chenchiapply@gmail.com>
2015-07-20 07:17:39 -0400

    Create EvaluateReversePolishNotation_001.py

---
+++ leetcode/150-Evaluate-Reverse-Polish-Notation/EvaluateReversePolishNotation_001.py
@@ -0,0 +1,28 @@
+#@author: cchen
+class Solution:
+    # @param {string[]} tokens
+    # @return {integer}
+    def evalRPN(self, tokens):
+        i = 0
+        while len(tokens) != 1:
+            if not tokens[i].strip('-').isdigit():
+                if tokens[i] == '*':
+                    tokens[i - 2] = str(int(tokens[i - 2]) * int(tokens[i - 1]))
+                if tokens[i] == '/':
+                    tmp1 = int(tokens[i - 2])
+                    tmp2 = int(tokens[i - 1])
+                    if tmp1 / tmp2 < 0 and tmp1 % tmp2 != 0:
+                        tokens[i - 2] = str(tmp1 / tmp2 + 1)
+                    else:
+                        tokens[i - 2] = str(tmp1 / tmp2)
+                if tokens[i] == '+':
+                    tokens[i - 2] = str(int(tokens[i - 2]) + int(tokens[i - 1]))
+                if tokens[i] == '-':
+                    tokens[i - 2] = str(int(tokens[i - 2]) - int(tokens[i - 1]))
+                del tokens[i - 1]
+                del tokens[i - 1]
+                i -= 1
+            else:
+                i += 1
+        
+        return int(tokens[0])
