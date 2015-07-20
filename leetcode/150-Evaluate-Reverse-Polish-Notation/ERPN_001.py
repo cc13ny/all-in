e015030d6dd10141353443d08c84285d00af438b
cyc <chenchiapply@gmail.com>
2015-07-20 07:20:50 -0400

    Update and rename EvaluateReversePolishNotation_001.py to ERPN_001.py

--- leetcode/150-Evaluate-Reverse-Polish-Notation/ERPN_001.py
+++ leetcode/150-Evaluate-Reverse-Polish-Notation/ERPN_001.py
@@ -5,10 +5,10 @@ class Solution:
     def evalRPN(self, tokens):
         i = 0
         while len(tokens) != 1:
-            if not tokens[i].strip('-').isdigit():
+            if not tokens[i].strip('-').isdigit(): # 1st prob in py, integer checker including negative ones
                 if tokens[i] == '*':
                     tokens[i - 2] = str(int(tokens[i - 2]) * int(tokens[i - 1]))
-                if tokens[i] == '/':
+                if tokens[i] == '/': # 2nd prob in py, ceiling func
                     tmp1 = int(tokens[i - 2])
                     tmp2 = int(tokens[i - 1])
                     if tmp1 / tmp2 < 0 and tmp1 % tmp2 != 0:
