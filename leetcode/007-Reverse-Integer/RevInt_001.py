29d2966eb8fbc9c79623cfaa88d318580c64fe19
cyc <chenchiapply@gmail.com>
2015-07-21 09:28:24 -0400

    Create RevInt_001.py

---
+++ leetcode/007-Reverse-Integer/RevInt_001.py
@@ -0,0 +1,24 @@
+#@author: cchen
+
+class Solution:
+    # @param {integer} x
+    # @return {integer}
+    
+    def isoverflow(self, s):
+        m = str(2**31 - 1)
+        if len(s) > len(m):
+            return True
+        if len(s) < len(m):
+            return False
+        if s > m:
+            return True
+        return False
+        
+    def reverse(self, x):
+        t = x if x > -1 else -x
+        s = str(t)
+        s = s[::-1]
+        res = 0 if self.isoverflow(s) else int(s)
+        res = res if x > -1 else -res
+                
+        return res
