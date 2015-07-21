87b10dd20fc168800db63978dcd6563e48333c88
cyc <chenchiapply@gmail.com>
2015-07-21 11:43:10 -0400

    Create ProdArrExcSelf_002.py

---
+++ leetcode/238-Product-of-Array-Except-Self/ProdArrExcSelf_002.py
@@ -0,0 +1,19 @@
+'''
+  Based on sol in http://www.cnblogs.com/grandyang/p/4650187.html
+  It's pretty awsesome in details so I have to comment them
+'''
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[]}
+    def productExceptSelf(self, nums):
+        res = [1] * len(nums) # The key point is that res[0] should be 1
+        for i in range(1, len(res)):
+            res[i] = res[i - 1] * nums[i - 1]
+        
+        right = 1 # Avoid using res[-1] in case of res == []
+        for j in range(len(res) - 1, -1, -1):
+            res[j] *= right
+            right *= nums[j]
+        
+        return res
