354288b0a05d16fba68ea8083f46d5678763c06b
cyc <chenchiapply@gmail.com>
2015-07-21 13:16:16 -0400

    Create ValParent_001.py

---
+++ leetcode/020-Valid-Parentheses/ValParent_001.py
@@ -0,0 +1,27 @@
+class Solution:
+    # @param {string} s
+    # @return {boolean}
+    def isValid(self, s):
+        l = ['(', '{', '[']
+        r = [')', '}', ']']
+        tb = {'(': ')', '{': '}', '[':']'}
+
+        if s == '':
+            return True
+    
+        if s[0] in r:
+            return False
+
+        ls = []
+        for i in range(len(s)):
+            if s[i] in l:
+                ls.append(s[i])
+            elif s[i] in r:
+                if len(ls) == 0:
+                    return False
+                end = ls.pop()
+                if s[i] != tb[end]:
+                    return False
+        if len(ls) != 0:
+            return False
+        return True
