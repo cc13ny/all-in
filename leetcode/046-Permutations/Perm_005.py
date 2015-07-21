b1e76d806ea095cde56574c5cb48d95a8a160b30
cyc <chenchiapply@gmail.com>
2015-07-21 13:46:51 -0400

    Create Perm_005.py

---
+++ leetcode/046-Permutations/Perm_005.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[][]}
+    def permute(self, nums):
+        if len(nums) == 1:
+            return [nums]
+            
+        res = []
+        for i in range(len(nums)):
+            tmp = nums[:i] + nums[i+1:]
+            sub = self.permute(tmp)
+            res.extend([[nums[i]] + s for s in sub])
+        
+        return res
