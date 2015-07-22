6813746424a3a10f27074bf018bd91aaeb3aafe7
cyc <chenchiapply@gmail.com>
2015-07-22 14:31:34 -0400

    Create MergeSortedArr_001.java

---
+++ leetcode/088-Merge-Sorted-Array/MergeSortedArr_001.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public void merge(int A[], int m, int B[], int n) {
+        int size = A.length;
+        int j = 0;
+        int k = 0;
+        int p = 0;
+        
+        
+        for(int i = m - 1; i > -1; i--){
+            A[i + size - m] = A[i];
+            
+        }
+        
+        while((j < m) && (k < n)){
+            if(A[j + size - m] <B[k]){
+                A[p] = A[j + size - m];
+                j++;
+                
+            }
+            else{
+                A[p] = B[k];
+                k++;
+            }
+            p++;
+        }
+        
+        
+        while(k < n){
+            A[p] = B[k];
+            p++;
+            k++;
+        }
+    }
+}
