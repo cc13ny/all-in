eb3510933b356c5b97e7a0cce9ebad563f21bf3c
cyc <chenchiapply@gmail.com>
2015-07-22 14:38:27 -0400

    Create BinTreeInTraversal_002.py

---
+++ leetcode/094-Binary-Tree-Inorder-Traversal/BinTreeInTraversal_002.py
@@ -0,0 +1,25 @@
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def iterative_inorder(self, root, list):
+        stack = []
+        while root or stack:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                list.append(root.val)
+                root = root.right
+        return list
+                
+    def recursive_inorder(self, root, list):
+        if root:
+            self.inorder(root.left, list)
+            list.append(root.val)
+            self.inorder(root.right, list)
+        
+    def inorderTraversal(self, root):
+        list = []
+        self.iterative_inorder(root, list)
+        return list
