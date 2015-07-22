4b9782e4dc430497b43249ef554a28aa8b9efbf4
cyc <chenchiapply@gmail.com>
2015-07-22 14:58:23 -0400

    Create BalancedBinTree_001.py

---
+++ leetcode/110-Balanced-Binary-Tree/BalancedBinTree_001.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param root, a tree node
+    # @return a boolean
+    def Height(self, root):
+        if root == None:
+            return 0
+        return max( self.Height( root.left ), self.Height( root.right ) ) + 1
+    
+    def isBalanced(self, root):
+        if root  == None:
+            return True
+        if abs( self.Height( root.left ) - self.Height( root.right ) ) <= 1:
+            return self.isBalanced( root.left ) and self.isBalanced( root.right )
+        else:
+            return False
