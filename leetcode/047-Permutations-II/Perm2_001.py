51b43d34a5789a3386da44633fec2fdc327c79e3
cyc <chenchiapply@gmail.com>
2015-07-22 10:58:32 -0400

    Create Perm2_001.py

---
+++ leetcode/047-Permutations-II/Perm2_001.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[][]}
+    def permuteUnique(self, nums):
+        if len(nums) == 0:
+            return nums
+        if len(nums) == 1:
+            return [nums]
+        
+        res = []
+        bag = set()
+        for i in range(len(nums)):
+            if nums[i] not in bag:
+                tmp = nums[:]
+                head = tmp.pop(i)
+                tail = self.permuteUnique(tmp)
+                [ t.insert(0, head) for t in tail]
+                res.extend(tail)
+                bag.add(nums[i])
+        
+        return res
