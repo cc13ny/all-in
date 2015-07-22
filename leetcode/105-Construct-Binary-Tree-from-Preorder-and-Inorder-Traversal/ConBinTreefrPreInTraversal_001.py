62d7609859f92a8e9a1187e2ea971136fbe68835
cyc <chenchiapply@gmail.com>
2015-07-22 14:53:44 -0400

    Create ConBinTreefrPreInTraversal_001.py

---
+++ leetcode/105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/ConBinTreefrPreInTraversal_001.py
@@ -0,0 +1,63 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+import java.util.Hashtable;
+
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int len = preorder.length;
+        
+        if(len == 0) return null;
+        
+        Hashtable<Integer, Integer> pretb = new Hashtable<Integer, Integer>();
+        Hashtable<Integer, Integer> intb = new Hashtable<Integer, Integer>();
+        
+        int root = preorder[0];
+        TreeNode tree_root = new TreeNode(root);
+        
+        for(int i = 0; i < len; i++){
+            pretb.put(preorder[i], i);
+            intb.put(inorder[i], i);
+        }
+        
+        
+        for(int j = 1; j < len; j++){
+            int num = preorder[j];
+            int inloc = intb.get(num);
+            TreeNode next = tree_root;
+            boolean flag = false;
+            while(true){
+                if(inloc < intb.get(next.val)){
+                    if(next.left == null){
+                        next.left = new TreeNode(num);
+                        flag = true;
+                    }
+                    else{
+                        next = next.left;
+                    }
+                }
+                else{
+                    if(next.right == null){
+                        next.right = new TreeNode(num);
+                        flag = true;
+                    }
+                    else{
+                        next = next.right;
+                    }
+                    
+                }
+                
+                if(flag) break;
+            }
+        }
+        
+        return tree_root;
+    }
+}
