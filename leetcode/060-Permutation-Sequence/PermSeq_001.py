c9f95137010c22175abf99850125e79a87b2166d
cyc <chenchiapply@gmail.com>
2015-07-22 11:23:01 -0400

    Create PermSeq_001.py

---
+++ leetcode/060-Permutation-Sequence/PermSeq_001.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param {integer} n
+    # @param {integer} k
+    # @return {string}
+    def getPermutation(self, n, k):
+        ls = range(1, n+1)
+        prod = 1
+        
+        for i in ls:
+            prod *= i
+        
+        res = ''
+        while n > 0:
+            prod = prod / n
+            idx = (k - 1)/prod
+        
+            num = ls.pop(idx)
+            res += str(num)
+            k -= prod * idx
+            n -= 1
+        return res
