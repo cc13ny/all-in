33a27a0e174cb2d62cceab43da47795dca183e30
cyc <chenchiapply@gmail.com>
2015-07-22 14:41:11 -0400

    Create UniqBinSearchTrees_001.py

---
+++ leetcode/096-Unique-Binary-Search-Trees/UniqBinSearchTrees_001.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param {integer} n
+    # @return {integer}
+    def numTrees(self, n):
+        a = [1] + [0] * n
+        for i in range(1, n+1):
+            for j in range(i):
+                a[i] += a[j] * a[i - 1 -j]
+        
+        return a[n]
