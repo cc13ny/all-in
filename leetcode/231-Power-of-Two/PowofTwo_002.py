7a6fc00db3e0b35ceb0b662159b73dbf61379c9d
cyc <chenchiapply@gmail.com>
2015-07-23 08:36:50 -0400

    Create PowofTwo_002.py

---
+++ leetcode/231-Power-of-Two/PowofTwo_002.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param {integer} n
+    # @return {boolean}
+    def isPowerOfTwo(self, n):
+        res = 0
+        while n > 0:
+            res += n & 1
+            n = n >> 1
+            
+        return res == 1
