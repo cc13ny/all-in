e40698034913acedaef386af5e44815455d63baf
cyc <chenchiapply@gmail.com>
2015-07-23 01:05:37 -0400

    Create ReverseWords_001.java

---
+++ leetcode/151-Reverse-Words-in-a-String/ReverseWords_001.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public String reverseWords(String s) {
+        String[] strs = s.trim().split("\\s+");
+        String newStr;
+        
+        if(strs.length > 0){
+            newStr = strs[strs.length -1];
+            for(int i = strs.length -2; i > -1; i--){
+                newStr = newStr + " " + strs[i];
+            }
+        }
+        else{
+            newStr = "";
+        }
+        return newStr;
+        
+    }
+}
