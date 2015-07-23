fc940a692c5d2aa79daf4018327cf92898b452a9
cyc <chenchiapply@gmail.com>
2015-07-23 01:38:11 -0400

    Create HouseRobber_002.py

---
+++ leetcode/198-House-Robber/HouseRobber_002.py
@@ -0,0 +1,25 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+        
+    def robaux(self, nums, aux):
+        i = len(nums) - 1
+    
+        if i < 0:
+            return 0
+    
+        if aux[i] > -1:
+            return aux[i]
+    
+        if i == 0:
+            return nums[0]
+
+        aux[i] = max(nums[0] + self.robaux(nums[2:], aux),
+                        nums[1] + self.robaux(nums[3:], aux))
+
+        return aux[i]
+
+    def rob(self, nums):
+        aux = [-1] * len(nums)
+
+        return self.robaux(nums, aux)
