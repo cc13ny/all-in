54a96cd333c6036abbd825c39f1b90450b3291d9
cyc <chenchiapply@gmail.com>
2015-07-23 01:11:09 -0400

    Create FindMininRSA_001.py

---
+++ leetcode/153-Find-Minimum-in-Rotated-Sorted-Array/FindMininRSA_001.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param num, a list of integer
+    # @return an integer
+    def findMin(self, num):
+        L = 0
+        R = len(num) - 1
+        
+        
+        while L < R and num[L] > num[R]:
+            M = (L + R)/2
+            if num[L] > num[M]:
+                R = M
+            else:
+                L = M + 1
+        return num[L]
