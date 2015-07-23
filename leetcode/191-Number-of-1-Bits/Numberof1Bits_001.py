69a5dd916e49b9323394dd61b7f8e0aa11f66ed6
cyc <chenchiapply@gmail.com>
2015-07-23 01:36:13 -0400

    Create Numberof1Bits_001.py

---
+++ leetcode/191-Number-of-1-Bits/Numberof1Bits_001.py
@@ -0,0 +1,13 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def hammingWeight(self, n):
+        a = bin(n)
+        b = a[2:]
+        c = b.split('0')
+        sm = 0
+        
+        for d in c:
+            sm += len(d)
+        
+        return sm
