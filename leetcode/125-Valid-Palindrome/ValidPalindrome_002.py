37548c9959fc8bcda46229c2970963e48e85d729
cyc <chenchiapply@gmail.com>
2015-07-22 21:01:32 -0400

    Create ValidPalindrome_002.py

---
+++ leetcode/125-Valid-Palindrome/ValidPalindrome_002.py
@@ -0,0 +1,13 @@
+class Solution:
+    # @param {string} s
+    # @return {boolean}
+    def isPalindrome(self, s):
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        i, j = 0, len(s) - 1
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        
+        return True
