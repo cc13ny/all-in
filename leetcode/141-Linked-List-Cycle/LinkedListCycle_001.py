bb78ca4021206d88d584f3d6f9a73ceddcac2bd8
cyc <chenchiapply@gmail.com>
2015-07-22 21:07:30 -0400

    Create LinkedListCycle_001.py

---
+++ leetcode/141-Linked-List-Cycle/LinkedListCycle_001.py
@@ -0,0 +1,13 @@
+class Solution:
+    # @param head, a ListNode
+    # @return a boolean
+    def hasCycle(self, head):
+        if head == None or head.next == None:
+            return False
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
