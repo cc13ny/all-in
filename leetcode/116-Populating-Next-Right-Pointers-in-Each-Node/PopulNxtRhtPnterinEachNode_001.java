78d75963d7652f9b09b4be2bde9e172c633e4e77
cyc <chenchiapply@gmail.com>
2015-07-22 20:47:22 -0400

    Create PopulNxtRhtPnterinEachNode_001.java

---
+++ leetcode/116-Populating-Next-Right-Pointers-in-Each-Node/PopulNxtRhtPnterinEachNode_001.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode parent = root;
+        if(parent != null){
+        
+	    	TreeLinkNode child = parent.left;
+	    	TreeLinkNode leftmostchild = child;
+	    	
+	    	while(leftmostchild != null){
+	    		if(child == parent.left){
+	    			child.next = parent.right;
+	    		}
+	    		else{
+	    			parent = parent.next;
+	    			if(parent != null){
+	    				child.next = parent.left;
+	    			}
+	    		}
+	    		child = child.next;
+	    		
+	    		if(child == null){
+	    			parent = leftmostchild;
+	    			child = parent.left;
+	    			leftmostchild = child;
+	    		}
+	    	}
+        }
+    }
+}
