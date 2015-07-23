5683bb795fc874e977a6922b2ad1d609ff779fe0
cyc <chenchiapply@gmail.com>
2015-07-23 01:39:46 -0400

    Create HouseRobber_003.py

---
+++ leetcode/198-House-Robber/HouseRobber_003.py
@@ -0,0 +1,9 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+
+    def rob(self, nums):
+        res = [0, 0] + nums 
+        for i in range(2, len(res)):
+            res[i] = max(res[i] + res[i-2], res[i-1])
+        return res[len(res)-1]
