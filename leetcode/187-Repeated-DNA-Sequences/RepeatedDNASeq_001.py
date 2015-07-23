e5a9e7557afe0f64bddb08d360279b8e711b254c
cyc <chenchiapply@gmail.com>
2015-07-23 01:28:27 -0400

    Create RepeatedDNASeq_001.py

---
+++ leetcode/187-Repeated-DNA-Sequences/RepeatedDNASeq_001.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param {string} s
+    # @return {string[]}
+    def findRepeatedDnaSequences(self, s):
+        if len(s) < 11:
+            return []
+        a = {}
+        for t in range(len(s) - 9):
+            if s[t:t+10] not in a:
+                a[s[t:t+10]] = 1
+            else:
+                a[s[t:t+10]] += 1
+        b = []
+        for i in a:
+            if a[i] > 1:
+                b.append(i)
+        return b
