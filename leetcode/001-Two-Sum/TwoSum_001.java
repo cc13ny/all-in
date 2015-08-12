1d44d936104880f621395c99388c691ca5261102
cyc <chenchiapply@gmail.com>
2015-08-12 00:28:31 -0400

    Update and rename TwoSum_002.java to TwoSum_001.java

--- leetcode/001-Two-Sum/TwoSum_001.java
+++ leetcode/001-Two-Sum/TwoSum_001.java
@@ -1,18 +1,13 @@
-/*
- * @author: cchen
- */
-
 import java.util.Hashtable;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         Hashtable<Integer, Integer> dict = new Hashtable<Integer, Integer>();
-        int len = nums.length;
         int[] res = new int[2];
-        for(int i=0; i<len; i++){
+        for(int i = 0; i < nums.length; i++){
             if(dict.containsKey(target - nums[i])){
-                res[0] = dict.get(target - nums[i])+1;
-                res[1] = i+1;
+                res[0] = dict.get(target - nums[i]) + 1;
+                res[1] = i + 1;
                 return res;
             }
             dict.put(nums[i], i);
