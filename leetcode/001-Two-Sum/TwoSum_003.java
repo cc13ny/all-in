9f41f934ca604ac4e14b14a6054d5879549bad27
cyc <chenchiapply@gmail.com>
2015-08-12 00:23:09 -0400

    Add Comparator for Getting Indices

--- leetcode/001-Two-Sum/TwoSum_003.java
+++ leetcode/001-Two-Sum/TwoSum_003.java
@@ -1,14 +1,28 @@
 import java.util.Arrays;
+import java.util.Comparator;
+
+class ArrayIndexComparator implements Comparator<Integer> {
+    private final int[] array;
+
+    public ArrayIndexComparator(int[] nums) {
+        array = nums;
+    }
+
+    public int compare(Integer index1, Integer index2) {
+        return array[index1] - array[index2];
+    }
+}
 
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
-        int[] idx = new int[nums.length];
+        Integer[] idx = new Integer[nums.length];
         for(int k = 0; k < idx.length; k++) {
             idx[k] = k;
         }
         
+        Arrays.sort(idx, new ArrayIndexComparator(nums));
         Arrays.sort(nums);
         
         int i = 0, j = nums.length - 1;
