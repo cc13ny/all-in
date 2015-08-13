a71d3b76cdba0879048e5cbfe7f6ee6fec1b9a78
cyc <chenchiapply@gmail.com>
2015-08-13 12:42:19 -0400

    Create MergekSortedLists_001.py

---
+++ leetcode/023-Merge-k-Sorted-Lists/MergekSortedLists_001.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+import heapq
+
+class Solution:
+    # @param {ListNode[]} lists
+    # @return {ListNode}
+    def mergeKLists(self, lists):
+        hq = []
+        for ls in lists:
+            if ls:
+                heapq.heappush(hq, (ls.val, ls))
+        dump = ListNode(0)
+        p = dump
+        while hq:
+            _, minnode = heapq.heappop(hq)
+            nextnode = minnode.next
+            if nextnode:
+                heapq.heappush(hq, (nextnode.val, nextnode))
+            p.next = minnode
+            p = p.next
+            
+        return dump.next
