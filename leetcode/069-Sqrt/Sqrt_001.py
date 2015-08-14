f5cc5fd43744c39f306d056586f8a3f972229b86
cyc <chenchiapply@gmail.com>
2015-08-14 14:53:14 -0400

    Create Sqrt_001.py

---
+++ leetcode/069-Sqrt/Sqrt_001.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param {integer} x
+    # @return {integer}
+    def mySqrt(self, x):
+        if x == 0:
+            return 0;
+        res, pre = 1.0, 0;
+        while res != pre:
+            pre = res
+            res = (res + x / res) / 2
+        return int(res)
