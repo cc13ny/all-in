10f74227b637f7da84dc513a3eea3292477fb70c
cyc <chenchiapply@gmail.com>
2015-08-14 15:08:51 -0400

    Create Searcha2DMatrix_001.py

---
+++ leetcode/074-Search-a-2D-Matrix/Searcha2DMatrix_001.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @param {integer[][]} matrix
+    # @param {integer} target
+    # @return {boolean}
+    def searchMatrix(self, matrix, target):
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+            
+        m , n = len(matrix), len(matrix[0])
+        
+        l, r = 0, m * n - 1
+        while l <= r:
+            mid = (l + r) / 2
+            i, j = mid / n, mid % n
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                l += 1
+            else:
+                r -= 1
+        
+        return False
