89f0082b709ab0baf734fa4c1c0b45bf6eb7e531
cyc <chenchiapply@gmail.com>
2015-08-14 09:15:18 -0400

    Create MaxSubarray_001.py

---
+++ leetcode/053-Maximum-Subarray/MaxSubarray_001.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def maxSubArray(self, nums):
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+    
+        minnum = nums[:]
+        for i in range(1, len(nums)):
+            minnum[i] = min(minnum[i - 1], nums[i - 1], 0)
+        
+        for i in range(1, len(nums)):
+            nums[i] -= minnum[i]
+        
+        return max(nums)
