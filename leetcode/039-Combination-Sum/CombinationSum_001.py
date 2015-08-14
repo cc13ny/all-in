dc7a0c0c2466a0d5018f9cb8a21a13d5be95d639
cyc <chenchiapply@gmail.com>
2015-08-13 20:04:26 -0400

    Create CombinationSum_001.py

---
+++ leetcode/039-Combination-Sum/CombinationSum_001.py
@@ -0,0 +1,26 @@
+class Solution:
+    # @param {integer[]} candidates
+    # @param {integer} target
+    # @return {integer[][]}
+    def combinationSum(self, candidates, target):
+        candidates.sort()
+        return self.combsum(candidates, target)
+        
+    def combsum(self, nums, target):
+        if target == 0:
+            return [[]]
+        if not nums or nums[0] > target or target < 1:
+            return []
+
+        res = []
+        for i in range(len(nums)):
+            num = nums[i]
+            pre = [num]
+            t = target
+            while t >= num:
+                t -= num
+                subs = self.combsum(nums[i + 1:], t)
+                for sub in subs:
+                    res.append(pre + sub)
+                pre += [num]
+        return res
