155e522f5bddcfb42eac2a22ce42be38a88e41bc
cyc <chenchiapply@gmail.com>
2015-08-15 10:38:38 -0400

    Create SearchinRSArrII_001.py

---
+++ leetcode/081-Search-in-Rotated-Sorted-Array-II/SearchinRSArrII_001.py
@@ -0,0 +1,25 @@
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {boolean}
+    def search(self, nums, target):
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            m = (l + r) / 2
+            if nums[m] == target:
+                return True
+            elif nums[m] < nums[r]:
+                if nums[m] < target and target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            elif nums[m] > nums[r]:
+                if nums[l] <= target and target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                r -= 1 # the only change
+        
+        return False
