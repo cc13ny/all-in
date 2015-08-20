62838049c0b97ff4b34f4df3d1c50036be00cee9
cyc <chenchiapply@gmail.com>
2015-08-20 16:19:59 -0400

    Create MajorityElementII_001.py

---
+++ leetcode/229-Majority-Element-II/MajorityElementII_001.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        c1, c2, num1, num2 = 0, 0, 0, 0
+        for c in nums:
+            if c == c1:
+                num1 += 1
+            elif c == c2:
+                num2 += 1
+            elif num1 == 0:
+                c1, num1 = c, 1
+            elif num2 == 0:
+                c2, num2 = c, 1
+            else:
+                num1 -= 1
+                num2 -= 1
+                
+        num1, num2 = 0, 0
+        for c in nums:
+            if c == c1:
+                num1 += 1
+            elif c == c2:
+                num2 += 1
+
+        res = []
+        if num1 > len(nums) / 3: res.append(c1)
+        if num2 > len(nums) / 3: res.append(c2)
+        
+        return res
