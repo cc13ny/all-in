16e7ba3ce5613c8a7b86de248be3ef13031aa2df
Chasego <chenchiapply@gmail.com>
2015-09-16 13:10:31 -0400

    Create BinTreePreTrav in NineChapter

---
+++ jiuzhang/Nine Chapters/3 Binary Tree & Divide Conquer/BinaryTreePreorderTraversal.py
@@ -0,0 +1,28 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorderTraversal(self, root):
+        # write your code here
+        arrlist, stack = [], [root]
+
+        while stack != [] and stack[0] is not None:
+            node = stack.pop()
+            while node is not None:
+                arrlist.append(node.val)
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            stack.append(node.right)
+
+        return arrlist
