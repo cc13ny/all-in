41521fd82c4ca960ef405a1f24f834660d56ea4c
Chasego <chenchiapply@gmail.com>
2015-09-18 20:03:11 -0400

    Create LowestCommonAncestor_iter.py for NineChap

--- jiuzhang/Nine Chapters/3 Binary Tree & Divide Conquer/LowestCommonAncestor_iter.py
+++ jiuzhang/Nine Chapters/3 Binary Tree & Divide Conquer/LowestCommonAncestor_iter.py
@@ -1,1 +1,43 @@
-# By using inorder
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+import copy
+class Solution:
+    """
+    @param root: The root of the binary search tree.
+    @param A and B: two nodes in a Binary.
+    @return: Return the least common ancestor(LCA) of the two nodes.
+    """ 
+    def lowestCommonAncestor(self, root, A, B):
+        # write your code here
+        dumpy = TreeNode(0)
+        dumpy.left = root
+        stack = [dumpy]
+
+        candicate, ancestor, ancestor_id = None, None, 0
+
+        while stack != [] and stack[0] is not None:
+            node = stack.pop()
+            while node is not None:
+                stack.append(node)
+                node = node.left
+            if len(stack) == ancestor_id:
+                ancestor = stack[-1]
+                ancestor_id -= 1
+            node = stack.pop()
+            if node == A or node == B:
+                if candicate is None:
+                    candicate = node
+                    ancestor = node
+                    ancestor_id = len(stack)
+                else:
+                    return ancestor
+        
+            stack.append(node.right)
+        if candicate:
+            return candicate
+        return None
