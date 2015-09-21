1244446dff32fc2d97813cdd8481d2530ca63538
Chasego <chenchiapply@gmail.com>
2015-09-21 13:46:03 -0400

    Create UniquePath.java for NineChap

---
+++ jiuzhang/Nine Chapters/4 Dynamic Programming I/java/UniquePaths.java
@@ -0,0 +1,22 @@
+public class Solution {
+    /**
+     * @param n, m: positive integer (1 <= n ,m <= 100)
+     * @return an integer
+     */
+    public int uniquePaths(int m, int n) {
+        // write your code here 
+        int[][] grid = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 || j == 0) {
+                    grid[i][j] = 1;
+                }
+                else {
+                    grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+                }
+            }
+        }
+        
+        return grid[m - 1][n - 1];
+    }
+}
