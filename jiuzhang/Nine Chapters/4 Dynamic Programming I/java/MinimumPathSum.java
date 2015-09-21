318afdfd990d9e7986eea34bb37adc951d8f56a8
Chasego <chenchiapply@gmail.com>
2015-09-21 17:37:51 -0400

    Create MinPathSum and Trian for NineChap

---
+++ jiuzhang/Nine Chapters/4 Dynamic Programming I/java/MinimumPathSum.java
@@ -0,0 +1,23 @@
+public class Solution {
+    /**
+     * @param grid: a list of lists of integers.
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
+    public int minPathSum(int[][] grid) {
+        // write your code here
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j != 0) {
+                    grid[i][j] += grid[i][j - 1];
+                } else if (i != 0 && j == 0) {
+                    grid[i][j] += grid[i - 1][j];
+                } else if (i != 0 && j != 0) {
+                    grid[i][j] += Math.min(grid[i][j - 1], grid[i - 1][j]);
+                }
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
---
+++ jiuzhang/Nine Chapters/4 Dynamic Programming I/java/Triangle.java
@@ -0,0 +1,34 @@
+public class Solution {
+    /**
+     * @param triangle: a list of lists of integers.
+     * @return: An integer, minimum path sum.
+     */
+    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
+        // write your code here
+        ArrayList<Integer> prelvl = triangle.get(0);
+        for (int i = 1; i < triangle.size(); i++) {
+            ArrayList<Integer> lvl = triangle.get(i);
+            for (int j = 0; j < lvl.size(); j++) {
+                int minsum = 0;
+                if (j == 0) {
+                    minsum = prelvl.get(j);
+                } else if (j == lvl.size() - 1) {
+                    minsum = prelvl.get(j - 1);
+                } else {
+                    minsum = Math.min(prelvl.get(j - 1), prelvl.get(j));
+                }
+                lvl.set(j, lvl.get(j) + minsum);
+            }
+            prelvl = lvl;
+        }
+        int endlen = triangle.size();
+        ArrayList<Integer> lastlvl = triangle.get(endlen - 1);
+        int res = lastlvl.get(0);
+        for (int k = 1; k < endlen; k++) {
+            if (lastlvl.get(k) < res) {
+                res = lastlvl.get(k);
+            }
+        }
+        return res;
+    }
+}
