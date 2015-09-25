64ad86e2dde2b3e882ed9b74a5d9232b928a2ed9
Chasego <chenchiapply@gmail.com>
2015-09-25 12:47:42 -0400

    Create Two Versions of WordBreak for Leetcode

---
+++ leetcode/139-Word-Break/WordBreak_001.py
@@ -0,0 +1,16 @@
+# {Leetcode : AC, Lintcode : TLE}
+
+class Solution:
+    # @param s, a string
+    # @param wordDict, a set<string>
+    # @return a boolean
+    def wordBreak(self, s, wordDict):
+        r = [True] + [False] * len(s)
+        wordDict = set(wordDict)
+        
+        for i in range(1, len(s) + 1):
+            for j in range(i + 1):
+                if r[j] and s[j : i] in wordDict:
+                    r[i] = True
+                    break
+        return r[-1]
