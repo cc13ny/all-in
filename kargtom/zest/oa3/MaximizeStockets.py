16b3dd0593194447542137a3f827731789b9479c
Chasego <chenchiapply@gmail.com>
2015-09-27 08:42:38 -0400

    Add Zest OA3

---
+++ kargtom/zest/oa3/MaximizeStockets.py
@@ -0,0 +1,10 @@
+def maxprofit(stockvalues): 
+    maxval, res = 0, 0
+    for i in range(len(stockvalues) - 1, -1, -1):
+        if maxval < stockvalues[i]:
+            maxval = stockvalues[i]
+        res += maxval - stockvalues[i]
+    return res
+
+stockvalues = [1, 3, 1, 2, 4]
+print "Profit was: " + str(maxprofit(stockvalues))
