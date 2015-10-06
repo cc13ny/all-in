794575f1326a382585d07c33142f3444b3509044
Chasego <chenchiapply@gmail.com>
2015-10-06 16:01:30 -0400

    Delete Node in Singly Linked List for Lintcode

---
+++ lintcode/237-372-Delete-Node-in-the-Middle-of-Singly-Linked-List/DeleteNodeintheMiddleofSinglyLinkedinList_001.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param node: the node in the list should be deleted
+     * @return: nothing
+     */
+    public void deleteNode(ListNode node) {
+        /*
+         * Replace the val of current node with the val of next node
+         * Connect the current node with the next next node of the current node
+         */
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
