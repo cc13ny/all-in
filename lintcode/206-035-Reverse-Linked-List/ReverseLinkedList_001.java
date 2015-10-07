bad4bdf0ee62c803eb74d861fe5e30b023b943cd
Chasego <chenchiapply@gmail.com>
2015-10-07 17:49:21 -0400

    Create ReverseLinkedList for LintCode

---
+++ lintcode/206-035-Reverse-Linked-List/ReverseLinkedList_001.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The head of linked list.
+     * @return: The new head of reversed linked list.
+     */
+    public ListNode reverse(ListNode head) {
+        // write your code here
+        if (head == null) {
+            return null;
+        }
+        ListNode dumpy = new ListNode(0);
+        dumpy.next = head;
+        ListNode p = head;
+        ListNode tmp;
+        
+        while (p.next != null) {
+            tmp = p.next;
+            p.next = tmp.next;
+            tmp.next = dumpy.next;
+            dumpy.next = tmp;
+        }
+        
+        return dumpy.next;
+    }
+}
