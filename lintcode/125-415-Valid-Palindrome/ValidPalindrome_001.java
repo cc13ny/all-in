875fdadc115512174f4e20e411427d4c37630b3e
Chasego <chenchiapply@gmail.com>
2015-10-07 13:56:34 -0400

    Create ValidPalindrome for LintCode

---
+++ lintcode/125-415-Valid-Palindrome/ValidPalindrome_001.java
@@ -0,0 +1,36 @@
+public class Solution {
+    /**
+     * @param s A string
+     * @return Whether the string is a valid palindrome
+     */
+    public boolean isPalindrome(String s) {
+        // Write your code here
+        if (s == null) {
+            return true;
+        }
+        int p = 0;
+        int q = s.length() - 1;
+        char left;
+        char right;
+
+        while (p < q) {
+            while (p < s.length() && !Character.isLetterOrDigit(s.charAt(p))) {
+                p++;
+            }
+            while (-1 < q && !Character.isLetterOrDigit(s.charAt(q))) {
+                q--;
+            }
+            if (p < q) {
+                left = Character.toLowerCase(s.charAt(p));
+                right = Character.toLowerCase(s.charAt(q));
+                if (left != right){
+                    return false;
+                }
+                p++;
+                q--;
+            }
+        }
+
+        return true;
+    }
+}
