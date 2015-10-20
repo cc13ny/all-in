a8bbd9cfc3bc1f287b8496c980cee0d51fa2b257
Chasego <chenchiapply@gmail.com>
2015-10-19 21:09:12 -0400

    Create SymX for Goodog

---
+++ talempd/goodog/2015/003-Symmetry-X/SymmetryX.py
@@ -0,0 +1,15 @@
+def sym_x_exist(points):
+    l, r = min(points), max(points)
+    mid_x = (l[0] + r[0]) / 2.0
+    pts = set(points)
+    for p in points:
+        if (2 * mid_x - p[0], p[1]) not in pts:
+            return False
+    return True
+
+tests = []
+tests.append([(1, 2), (-1, 2), (3, 4), (-3, 4)])
+tests.append([(1, 2), (-1, 2), (3, 4), (3, 5)])
+
+for test in tests:
+    print sym_x_exist(test)
