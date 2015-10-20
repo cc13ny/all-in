670f5c2ed445a7e4f9ee516cfd1afea89d90a58f
Chasego <chenchiapply@gmail.com>
2015-10-20 15:44:02 -0400

    Create Excel Col-Num-Title Conversion for Goodog

---
+++ talempd/goodog/2015/007-Excel-Col-Num-Conversion/ExcelSheetColNum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = 0
+        for i in range(len(s) - 1, -1, -1):
+            res += (ord(s[i]) - ord('A') + 1) * 26 ** (len(s) - 1 - i)
+        
+        return res
