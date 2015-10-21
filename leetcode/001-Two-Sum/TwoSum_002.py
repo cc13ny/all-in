62a96fd9dba12e5185b73fa33b0b7b07c3eaf9c4
cyc <chenchiapply@gmail.com>
2015-10-21 09:41:08 -0400

    Update TwoSum_002.py

--- leetcode/001-Two-Sum/TwoSum_002.py
+++ leetcode/001-Two-Sum/TwoSum_002.py
@@ -4,17 +4,17 @@ class Solution:
     # @param {integer} target
     # @return {integer[]}
     def twoSum(self, nums, target):
-        idx = sorted(range(len(nums)), key=lambda x:nums[x])
+        idx = sorted(range(len(nums)), key = lambda x:nums[x])
         nums.sort()
         i, j = 0, len(nums)-1
         
         while i < j:
-            if nums[i]+nums[j]>target:
-                j-=1
-            elif nums[i]+nums[j]<target:
-                i+=1
+            if nums[i] + nums[j] > target:
+                j -= 1
+            elif nums[i] + nums[j] < target:
+                i += 1
             else:
                 if idx[i] > idx[j]:
-                    return [idx[j]+1, idx[i]+1]
+                    return [idx[j] + 1, idx[i] + 1]
                 else:
-                    return [idx[i]+1, idx[j]+1]
+                    return [idx[i] + 1, idx[j] + 1]
