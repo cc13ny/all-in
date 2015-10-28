45a9c2b3b8e0033c6dca5b87d52f515d23a9356c
Chasego <chenchiapply@gmail.com>
2015-10-28 14:13:59 -0400

    Create Combination like 001 for Leetcode

---
+++ leetcode/077-Combinations/Combinations_002.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        if n < 1:
+            return []
+        
+        if k == 1:
+            return [[i] for i in range(1, n + 1)]
+        
+        res = self.combine(n - 1, k)
+        tmp = self.combine(n - 1, k - 1)
+        for t in tmp:
+            res.append(t + [n])
+        return res
