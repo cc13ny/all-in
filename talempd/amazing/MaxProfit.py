d3367d32dbc080d8e963542351c427fe2da48f18
Chasego <chenchiapply@gmail.com>
2015-11-13 13:56:02 -0500

    Change name for maxprofit in amazing

---
+++ talempd/amazing/MaxProfit.py
@@ -0,0 +1,9 @@
+def maxprofit(stockvals): 
+    maxval, profit = 0, 0
+    for stockval in stockvals[::-1]:
+        maxval = max(maxval, stockval)
+        profit += maxval - stockval
+    return profit
+
+stockvalues = [1, 3, 1, 2, 4]
+print "Profit was: " + str(maxprofit(stockvalues))
