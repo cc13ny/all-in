fab75da3841eec10cd2b31bf78f2ae9322c9f12b
cyc <chenchiapply@gmail.com>
2015-11-30 11:48:21 -0500

    Create LongSubWRC_003.java

---
+++ leetcode/003-Longest-Substring-Without-Repeating-Characters/LongSubWRC_003.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> mark = new HashMap<Character, Integer>();
+        char c;
+        int i = -1;
+        int res = 0, tmp = 0;
+        
+        for (int j = 0; j < s.length(); j++) {
+            c = s.charAt(j);
+            if (mark.containsKey(c) && mark.get(c) > i) {
+                if (tmp > res) {
+                    res = tmp;
+                }
+                tmp = j - mark.get(c);
+                i = mark.get(c);
+                
+            } else {
+                tmp++;
+            }
+            
+            mark.put(c, j);
+        }
+        
+        if (tmp > res) {
+            res = tmp;
+        }
+        
+        return res;
+    }
+}
