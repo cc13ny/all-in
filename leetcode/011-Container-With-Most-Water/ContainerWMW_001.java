da0a1db7a052ed80448e4821d00978c92ad0ea9a
cyc <chenchiapply@gmail.com>
2015-11-30 21:58:59 -0500

    Create ContainerWMW_001.java

---
+++ leetcode/011-Container-With-Most-Water/ContainerWMW_001.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int size = height.length;
+        int i = 0, j = size - 1;
+        int res = 0, tmp = 0, h = 0;
+        
+        while (i < j) {
+            tmp = Math.min(height[i], height[j]) * (j - i);
+            if (tmp > res) {
+                res = tmp;
+            }
+            if (height[i] < height[j]) {
+                h = height[i];
+                while (i < size && height[i] <= h) {
+                    i++;
+                }
+            } else {
+                h = height[j];
+                while (-1 < j && height[j] <= h) {
+                    j--;
+                }
+            }
+        }
+        
+        return res;
+    }
+}
