c5d4cc5c40e31c48a4846a1ed5c503a33b251209
cyc <chenchiapply@gmail.com>
2015-11-30 21:29:10 -0500

    Create PalNum_002.java

---
+++ leetcode/009-Palindrome-Number/PalNum_002.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        
+        int t = x;
+        int lloc = 1, rloc = 1;
+        
+        while (t > 9) {
+            t /= 10;
+            rloc *= 10;
+        }
+
+        int lnum = 0, rnum = 0;
+        while (lloc < rloc) {
+            lnum = (x / lloc) % 10;
+            rnum = (x / rloc) % 10;
+            
+            if (lnum != rnum) {
+                return false;
+            }
+            
+            lloc *= 10;
+            rloc /= 10;
+        }
+        
+        return true;
+    }
+}
