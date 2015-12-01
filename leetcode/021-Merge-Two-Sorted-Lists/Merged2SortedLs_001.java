dbbc327689e109adb47d7278d395f2bc86c8a4a2
cyc <chenchiapply@gmail.com>
2015-12-01 15:38:13 -0500

    Create Merged2SortedLs_001.java

---
+++ leetcode/021-Merge-Two-Sorted-Lists/Merged2SortedLs_001.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dumpy = new ListNode(0);
+        ListNode p = dumpy;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        
+        if (l1 == null) {
+            p.next = l2;
+        }
+        
+        if (l2 == null) {
+            p.next = l1;
+        }
+        
+        return dumpy.next;
+    }
+}
