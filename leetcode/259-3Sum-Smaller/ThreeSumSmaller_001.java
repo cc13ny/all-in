6e92cb42626ee81648af87fe7159181210c856f6
cyc <chenchiapply@gmail.com>
2015-12-02 16:21:15 -0500

    Create ThreeSumSmaller_001.java

---
+++ leetcode/259-3Sum-Smaller/ThreeSumSmaller_001.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        Arrays.sort(nums);
+        int size = nums.length, res = 0;
+        for (int i = 0; i < size - 2; i++) {
+            int newtarget = target - nums[i];
+            int j = i + 1, k = size - 1;
+            while (j < k) {
+                int twosum = nums[j] + nums[k];
+                if (twosum < newtarget) {
+                    res += k - j;
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        
+        return res;
+    }
+}
