a2c547d190202291416fda632edc3369f31cf117
cyc <chenchiapply@gmail.com>
2015-12-02 18:24:02 -0500

    Create FirstBadVersion_001.java

---
+++ leetcode/278-First-Bad-Version/FirstBadVersion_001.java
@@ -0,0 +1,18 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l = 1, r = n;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (isBadVersion(m)) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        
+        return l;
+    }
+}
