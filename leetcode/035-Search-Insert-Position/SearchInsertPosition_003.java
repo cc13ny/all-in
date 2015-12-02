7216c48d8ba4057672a5f56ba171a54c0f2e2151
cyc <chenchiapply@gmail.com>
2015-12-02 13:59:34 -0500

    Create SearchInsertPosition_003.java

---
+++ leetcode/035-Search-Insert-Position/SearchInsertPosition_003.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        
+        return l;
+    }
+}
