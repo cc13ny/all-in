a1d76eceee5d0e9e3a95964fb684eddf71e28b28
cyc <chenchiapply@gmail.com>
2015-12-02 18:24:57 -0500

    Update FirstBadVersion_001.java

--- leetcode/278-First-Bad-Version/FirstBadVersion_001.java
+++ leetcode/278-First-Bad-Version/FirstBadVersion_001.java
@@ -5,7 +5,8 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int l = 1, r = n;
         while (l <= r) {
-            int m = l + (r - l) / 2; // key point: avoid overflow which causes strange behavior of m
+            // key point: avoid overflow which causes strange behavior of m
+            int m = l + (r - l) / 2; 
             if (isBadVersion(m)) {
                 r = m - 1;
             } else {
