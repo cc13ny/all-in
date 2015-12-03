f00f555200f0abba12a90ce5e0903b6ab9c5a2fc
cyc <chenchiapply@gmail.com>
2015-12-03 11:08:34 -0500

    Create ValAnagram_003.py

---
+++ leetcode/242-Valid-Anagram/ValAnagram_003.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param {string} s
+    # @param {string} t
+    # @return {boolean}
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        m = [0] * 128
+        for c in s:
+            m[ord(c)] += 1
+        
+        for c in t:
+            m[ord(c)] -= 1
+            if m[ord(c)] < 0:
+                return False
+        return True
