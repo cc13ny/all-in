d4fbc3dac36b32eb13e28d6c57ff8dd3e68b397e
cyc <chenchiapply@gmail.com>
2015-12-03 12:00:34 -0500

    Create ValidPalindrome_002.java

---
+++ leetcode/125-Valid-Palindrome/ValidPalindrome_002.java
@@ -0,0 +1,43 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        
+        while (i < j) {
+            char chari = s.charAt(i);
+            char charj = s.charAt(j);
+            
+            while (!Character.isLetterOrDigit(chari)) {
+                i++;
+                if (i < j) {
+                    chari = s.charAt(i);
+                } else {
+                    break;
+                }
+            }
+            
+            while (!Character.isLetterOrDigit(charj)) {
+                j--;
+                if (i < j) {
+                    charj = s.charAt(j);
+                } else {
+                    break;
+                }
+            }
+            
+            if (i < j) {
+                char chi = Character.toLowerCase(chari);
+                char chj = Character.toLowerCase(charj);
+                if (chi != chj) {
+                    return false;
+                } else {
+                    i++;
+                    j--;
+                }
+            } else {
+                break;
+            }
+        }
+        
+        return true;
+    }
+}
