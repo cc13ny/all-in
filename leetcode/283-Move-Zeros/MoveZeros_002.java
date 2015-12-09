a45f46610295f898d2bdfb9683dd5ac46f7209da
cyc <chenchiapply@gmail.com>
2015-12-09 18:30:13 -0500

    Create MoveZeros_002.java

---
+++ leetcode/283-Move-Zeros/MoveZeros_002.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = -1;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (j < 0 && nums[i] == 0) {
+                j = i;
+            }
+            
+            if(j >= 0 && nums[i] != 0) {
+                nums[j] = nums[i];
+                nums[i] = 0;
+                j++;
+            }
+        }
+    }
+}
