8068ff4deeaa754562ab147ef4d74498d2e5021f
cyc <chenchiapply@gmail.com>
2015-12-09 20:30:59 -0500

    Create ClimbingStairs_001.java

---
+++ leetcode/070-Climbing-Stairs/ClimbingStairs_001.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int climbStairs(int n) {
+        int pre = 1, cur = 1;
+        
+        for (int i = 0; i < n - 1; i++) {
+            cur += pre;
+            pre = cur - pre;
+        }
+        return cur;
+    }
+}
