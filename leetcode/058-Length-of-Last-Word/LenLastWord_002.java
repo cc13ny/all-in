ac124f7243eb3e06b0e937abae940f640d1b7c3d
cyc <chenchiapply@gmail.com>
2015-12-09 19:45:00 -0500

    Create LenLastWord_002.java

---
+++ leetcode/058-Length-of-Last-Word/LenLastWord_002.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int res = 0;
+        for (int i = s.length() - 1; i > -1; i--) {
+            if (res == 0) {
+                if (s.charAt(i) != ' ') {
+                    res++;
+                }
+            } else {
+                if (s.charAt(i) != ' ') {
+                    System.out.println(i);
+                    res++;
+                } else {
+                    return res;
+                }
+            }
+        }
+        
+        return res;
+    }
+}
