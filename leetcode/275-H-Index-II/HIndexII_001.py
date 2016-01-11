5d2c2d2dca4301f2e3323a97feb92f3daba885d8
cyc <chenchiapply@gmail.com>
2016-01-11 17:51:15 -0500

    Create HIndexII_001.py

---
+++ leetcode/275-H-Index-II/HIndexII_001.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        
+        res = 0
+        n = len(citations)
+        l, r = 0, n - 1
+        while l <= r:
+            m = l + (r - l) / 2
+            if citations[m] >=  n - m:
+                r = m - 1
+            else:
+                l = m + 1
+        return n - l
