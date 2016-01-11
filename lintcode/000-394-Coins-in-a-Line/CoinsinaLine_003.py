20c5535f6c0fae681588545dcca33e57eb3961ad
cyc <chenchiapply@gmail.com>
2016-01-11 13:16:21 -0500

    Update CoinsinaLine_003.py

--- lintcode/000-394-Coins-in-a-Line/CoinsinaLine_003.py
+++ lintcode/000-394-Coins-in-a-Line/CoinsinaLine_003.py
@@ -3,6 +3,4 @@ class Solution:
     # @return: a boolean which equals to True if the first player will win
     def firstWillWin(self, n):
         # write your code here
-        if n % 3 == 0:
-            return False
-        return True
+        return not (n % 3 == 0)
