e18ebc48a0d50a4bef3663dff33b28b1acef5f49
cyc <chenchiapply@gmail.com>
2016-01-12 01:50:53 -0500

    Create ConvSortedArr2BST_001_rec.py

---
+++ leetcode/108-Convert-Sorted-Array-to-Binary-Search-Tree/ConvSortedArr2BST_001_rec.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if nums == []:
+            return None
+        m = (len(nums) - 1) / 2
+        root = TreeNode(nums[m])
+        root.left = self.sortedArrayToBST(nums[:m])
+        root.right = self.sortedArrayToBST(nums[m + 1:])
+        return root
