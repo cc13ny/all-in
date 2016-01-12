453df6abe7741fe0f24c03754b26c197fa282656
cyc <chenchiapply@gmail.com>
2016-01-12 00:58:29 -0500

    Create ValidateBST_002_iter.py

---
+++ leetcode/098-Validate-Binary-Search-Tree/ValidateBST_002_iter.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        stack = [root]
+        pre = None
+        
+        while stack != [] and stack[0]:
+            p = stack.pop()
+            while p:
+                stack.append(p)
+                p = p.left
+            p = stack.pop()
+            if pre and pre.val >= p.val:
+                return False
+            pre = p
+            stack.append(p.right)
+            
+        return True
