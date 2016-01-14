f2f2c88fb2db4c1e0f61fd03c66b18e2413e6ce2
cyc <chenchiapply@gmail.com>
2016-01-13 22:25:28 -0500

    Create JumpGame_002.py

---
+++ leetcode/055-Jump-Game/JumpGame_002.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def canJump(self, nums):
+        n = len(nums)
+        nearestrue = n - 1
+        
+        for i in range(n - 1, -1, -1):
+            if i + nums[i] >= nearestrue:
+                nearestrue = i
+        return nearestrue == 0
