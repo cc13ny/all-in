2a963c4d13035b6f8e301a7f0240b28e0e0764d3
cyc <chenchiapply@gmail.com>
2016-01-14 13:03:40 -0500

    Create WordLadder_001.py

---
+++ leetcode/127-Word-Ladder/WordLadder_001.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: Set[str]
+        :rtype: int
+        """
+        if beginWord == endWord:
+            return 1
+        
+        cnt = 1
+        q = [beginWord]
+        
+        while q:
+            nq = []
+            for word in q:
+                for i in range(len(beginWord)):
+                    for j in 'abcdefghijklmnopqrstuvwxyz':
+                        if j != word[i]:
+                            nword = word[:i] + j + word[i + 1:]
+                            if nword == endWord:
+                                return cnt + 1
+                            if nword in wordList:
+                                nq.append(nword)
+                                wordList.remove(nword)
+            cnt += 1
+            q = nq
+        return 0
