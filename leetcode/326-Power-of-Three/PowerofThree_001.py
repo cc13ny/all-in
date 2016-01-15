f68b1a9d5aa2c36f9301588a55bc217a9ed120c1
cyc <chenchiapply@gmail.com>
2016-01-15 10:51:02 -0500

    Create PowerofThree_001.py

---
+++ leetcode/326-Power-of-Three/PowerofThree_001.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and 3 ** round(math.log(n, 3)) == n
