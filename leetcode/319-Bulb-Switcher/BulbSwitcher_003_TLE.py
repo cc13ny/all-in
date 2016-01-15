87d16d9a8fd7ecba2549e54c16eec386eafa8494
cyc <chenchiapply@gmail.com>
2016-01-15 16:24:47 -0500

    Create BulbSwitcher_003_TLE.py

---
+++ leetcode/319-Bulb-Switcher/BulbSwitcher_003_TLE.py
@@ -0,0 +1,23 @@
+def bulbSwitch(n):
+    res = 1
+    primes = []
+    for j in range(2, n + 1):
+        m = j / 2
+        case = 0
+        for p in primes:
+            cnt = 0
+            if p > m:
+               break
+            elif j % p == 0:
+                case = 1
+                while t % p == 0:
+                    t /= p
+                    cnt += 1
+                if cnt % 2 == 1:
+                    case = 2
+                    break
+        if case == 0:
+            primes.append(j)
+        if case == 1:
+            res += 1
+    return res
