56b574863b9d4684d0192c86ff2bf584ae511c35
cyc <chenchiapply@gmail.com>
2016-01-17 22:23:09 -0500

    Create NimGame_002.py

---
+++ leetcode/292-Nim-Game/NimGame_002.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        dp = [False] * 4
+        cur = 0
+        for i in range(n):
+            dp[-1] = (not dp[0]) or (not dp[1]) or (not dp[2])
+            dp[cur] = dp[-1]
+            cur = (cur + 1) % 3
+        
+        return dp[-1]
