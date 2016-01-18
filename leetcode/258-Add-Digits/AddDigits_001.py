6277cdba1156593e6b258bd3e5b148d815c2bce9
cyc <chenchiapply@gmail.com>
2016-01-17 22:42:16 -0500

    Update AddDigits_001.py

--- leetcode/258-Add-Digits/AddDigits_001.py
+++ leetcode/258-Add-Digits/AddDigits_001.py
@@ -1,9 +1,5 @@
-# Ref: https://en.wikipedia.org/wiki/Digital_root
-
 class Solution:
     # @param {integer} num
     # @return {integer}
     def addDigits(self, num):
-        if num == 0:
-            return 0
-        return num - (num - 1) / 9 * 9
+        return num - (num - 1) / 9 * 9 if num else 0
