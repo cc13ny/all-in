9604ad01af22b9b0c7909fb4a66bd4cbc26288a8
cyc <chenchiapply@gmail.com>
2016-01-17 21:53:11 -0500

    Create LongComPrefix_003.py

---
+++ leetcode/014-Longest-Common-Prefix/LongComPrefix_003.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs: return ""
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != strs[j - 1][i]:
+                    return strs[0][:i]
+        return strs[0]
