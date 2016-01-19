3559faeceff06aee82409ca22158223aff696b07
cyc <Chasego@users.noreply.github.com>
2016-01-19 10:38:27 -0500

    Create MajorityElement_004.py

---
+++ leetcode/169-Majority-Element/MajorityElement_004.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num, cnt = nums[0], 1
+        for i in xrange(1, len(nums)):
+            if nums[i] == num:
+                cnt += 1
+            elif cnt == 0:
+                num = nums[i]
+                cnt = 1
+            else:
+                cnt -= 1
+        return num
