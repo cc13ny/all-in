b59ee00fdd32b9c734fe91faaa7dda6b19959ad2
cyc <Chasego@users.noreply.github.com>
2016-01-21 21:17:50 -0500

    Create Numberof1Bits_002.py

---
+++ leetcode/191-Number-of-1-Bits/Numberof1Bits_002.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        while n:
+            res += n & 1
+            n = n >> 1
+        return res
