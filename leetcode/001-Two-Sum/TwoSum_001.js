82c4c26f91bc7a874ee1eed5dce5b82febea6444
cyc <Chasego@users.noreply.github.com>
2016-01-22 14:36:13 -0500

    Create TwoSum_001.js

---
+++ leetcode/001-Two-Sum/TwoSum_001.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var num, i = 0, map = {};
+    for (num of nums) {
+        if (target - num in map) {
+            return [map[target - num] + 1, i + 1];
+        }
+        map[num] = i;
+        i++;
+    }
+};
