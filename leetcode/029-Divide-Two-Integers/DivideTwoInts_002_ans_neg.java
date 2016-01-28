7ed958a4f9ce3bd37b677131dfa6fa26ced01d1b
Chasego <chenchiapply@gmail.com>
2016-01-27 21:23:58 -0500

    Add DividTwoInt for leetcode

---
+++ leetcode/029-Divide-Two-Integers/DivideTwoInts_002_ans_neg.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        
+        if (dividend == Integer.MIN_VALUE && divisor == 1) {
+            return Integer.MIN_VALUE;
+        }
+        
+        boolean isdiff = (dividend > 0 && divisor < 0) || 
+                         (dividend < 0 && divisor > 0);
+        long a = - Math.abs((long)dividend);
+        long b = - Math.abs((long)divisor);
+        
+        int res = 0;
+        while (a <= b) {
+            int shift = 0;
+            while (a <= (b << shift)) {
+                shift++;
+            }
+            
+            a -= b << (shift - 1);
+            res += 1 << (shift - 1);
+        }
+        return isdiff ? -res : res;
+    }
+}
---
+++ leetcode/029-Divide-Two-Integers/README.md
@@ -0,0 +1,5 @@
+ToDo
+-------
+
+1. Last Corner Case for Python codes
+2. Have to use Long
