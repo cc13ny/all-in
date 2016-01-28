a9c1654156b55dc2575efab1b100cccbb8b9347f
cyc <Chasego@users.noreply.github.com>
2016-01-28 18:09:03 -0500

    Create PalNum_003_tle.java

---
+++ leetcode/009-Palindrome-Number/PalNum_003_tle.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        
+        int left = 10;
+        while (left <= x) {
+            left *= 10;
+        }
+        left /= 10;
+
+        while (x != 0) {
+            int ldigit = x / left;
+            int rdigit = x % 10;
+            if (ldigit != rdigit) {
+                return false;
+            }
+            x = (x % left) / 10;
+            left  /= 100;
+        }
+        
+        return true;
+    }
+}
