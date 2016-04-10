c657d92f1f8dc3cd4ff9995dc0d2857ce8f6fdd4
cyc <cc13ny@users.noreply.github.com>
2016-04-10 01:47:16 -0400

    Create CountingBits.py

---
+++ leetcode/338-Counting-Bits/CountingBits.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        seed = 1
+        res = [0]
+        
+        while num > 0:
+            res += [res[i] + 1 for i in xrange(min(num, seed))]
+            num -= seed
+            seed = seed << 1
+        return res
