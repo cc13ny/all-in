69f323bba974ea73963c8da63a3c3b8326fffc6e
cyc <cc13ny@users.noreply.github.com>
2016-04-19 02:07:01 -0400

    Create RevLinkedList_002.py

---
+++ leetcode/206-Reverse-Linked-List/RevLinkedList_002.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        
+        while head and head.next:
+            p = head.next
+            head.next = p.next
+            p.next = dummy.next
+            dummy.next = p
+            
+        return dummy.next
