d59eeee77807f417273f6615135cb36f0ded76e6
cyc <cc13ny@users.noreply.github.com>
2016-04-26 14:29:51 -0400

    Create ReverseVowelsofaString.py

---
+++ leetcode/345-Reverse-Vowels-of-a-String/ReverseVowelsofaString.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        
+        ls = list(s)
+        idx = [i for i in xrange(len(s)) if s[i] in vowels]
+        
+        for i in xrange(len(idx) / 2):
+            l, r = idx[i], idx[-i-1]
+            ls[l], ls[r] = ls[r], ls[l]
+        return ''.join(ls)
