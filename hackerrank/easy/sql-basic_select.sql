0f2b2a93f9668111492092f23f507d652094ae0a
cyc <cc13ny@users.noreply.github.com>
2016-06-05 16:47:30 -0400

    Create sql-basic_select.sql

---
+++ hackerrank/easy/sql-basic_select.sql
@@ -0,0 +1,15 @@
+/* https://www.hackerrank.com/challenges/revising-the-select-query */
+
+select * from city where countrycode = 'USA' and population > 100000;
+
+/* https://www.hackerrank.com/challenges/revising-the-select-query-2 */
+
+select name from city where countrycode = 'USA' and population > 120000;
+
+/* https://www.hackerrank.com/challenges/select-all-sql */
+
+select * from city;
+
+/*https://www.hackerrank.com/challenges/select-by-id*/
+
+select * from city where id = 1661;
