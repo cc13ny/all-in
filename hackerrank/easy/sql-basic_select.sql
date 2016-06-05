58b51b43552ac221926e16adf94f3a598e53be43
cyc <cc13ny@users.noreply.github.com>
2016-06-05 16:50:12 -0400

    Add selection for JPN city

--- hackerrank/easy/sql-basic_select.sql
+++ hackerrank/easy/sql-basic_select.sql
@@ -1,15 +1,19 @@
-/* https://www.hackerrank.com/challenges/revising-the-select-query */
+/*https://www.hackerrank.com/challenges/revising-the-select-query*/
 
 select * from city where countrycode = 'USA' and population > 100000;
 
-/* https://www.hackerrank.com/challenges/revising-the-select-query-2 */
+/*https://www.hackerrank.com/challenges/revising-the-select-query-2*/
 
 select name from city where countrycode = 'USA' and population > 120000;
 
-/* https://www.hackerrank.com/challenges/select-all-sql */
+/*https://www.hackerrank.com/challenges/select-all-sql*/
 
 select * from city;
 
 /*https://www.hackerrank.com/challenges/select-by-id*/
 
 select * from city where id = 1661;
+
+/*https://www.hackerrank.com/challenges/japanese-cities-detail*/
+
+select * from city where countrycode = 'JPN';
