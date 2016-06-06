171e21d5b2c2c3af2d173860bc53c031d0ad67e7
cyc <cc13ny@users.noreply.github.com>
2016-06-05 21:37:21 -0400

    Update sql-basic_select.sql

--- hackerrank/easy/sql-basic_select.sql
+++ hackerrank/easy/sql-basic_select.sql
@@ -17,3 +17,9 @@ select * from city where id = 1661;
 /*https://www.hackerrank.com/challenges/japanese-cities-detail*/
 
 select * from city where countrycode = 'JPN';
+
+/*https://www.hackerrank.com/challenges/weather-observation-station-5*/
+
+/*Can do it better!!!*/
+select city, length(city) from station order by length(city) asc limit 1; 
+select city, length(city) from station order by length(city) desc limit 1; 
