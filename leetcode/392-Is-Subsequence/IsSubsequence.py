80ebe11175cf52f02afa1d3d0aa4c73e7069a731
cyc <cc13ny@users.noreply.github.com>
2016-09-05 18:16:55 -0400

    Create IsSubsequence.py

---
+++ leetcode/392-Is-Subsequence/IsSubsequence.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) > len(t):
+            return False
+        
+        if s == '':
+            return True
+            
+        i = 0
+        n = len(s)
+        for c in t:
+            if c == s[i]:
+                i += 1
+                if i == n:
+                    return True
+        return False
