efd779ec837327270ba38391b103d3287b697c8b
cyc <cc13ny@users.noreply.github.com>
2016-09-20 12:57:04 -0400

    Update RemoveKDigits_tle.py

--- leetcode/402-Remove-K-Digits/RemoveKDigits_tle.py
+++ leetcode/402-Remove-K-Digits/RemoveKDigits_tle.py
@@ -9,25 +9,25 @@ class Solution(object):
         idx, kk = l, k
         res = ''
         while len(res) != len(num) - kk:
-        	idx = l
-        	tmp = num[l]
-        	for i in xrange(l, min(r, len(num) - 1) + 1):
-        		if num[i] == 0:
-        			idx = i
-        			break
-        		if num[i] < tmp:
-        			idx = i
-        			tmp = num[i]
-        	k -= (idx - l)
-        	l = idx + 1
-        	r = l + k
-        	res += num[idx]
+            idx = l
+            tmp = num[l]
+            for i in xrange(l, min(r, len(num) - 1) + 1):
+            	if num[i] == 0:
+            		idx = i
+            		break
+            	if num[i] < tmp:
+            		idx = i
+            		tmp = num[i]
+            k -= (idx - l)
+            l = idx + 1
+            r = l + k
+            res += num[idx]
         if k == 0:
-        	res += num[idx + 1:]
+            res += num[idx + 1:]
         idx = -1
         for i in xrange(len(res)):
-        	if res[i] != '0':
-        		idx = i
-        		break
+            if res[i] != '0':
+                idx = i
+                break
         res = '0' if idx == -1 else res[idx:]
         return res
