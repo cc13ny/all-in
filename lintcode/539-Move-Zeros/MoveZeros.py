aba498de8edb990e63f0b6759229f416ae8fc3d5
cyc <cc13ny@users.noreply.github.com>
2016-10-25 23:23:36 -0400

    Create MoveZeros.py

---
+++ lintcode/539-Move-Zeros/MoveZeros.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param {int[]} nums an integer array
+    # @return nothing, do this in-place
+    def moveZeroes(self, nums):
+        # Write your code here
+        i = 0
+        for j in xrange(len(nums)):
+           if nums[j]:
+               num = nums[j]
+               nums[j] = 0
+               nums[i] = num
+               i += 1
