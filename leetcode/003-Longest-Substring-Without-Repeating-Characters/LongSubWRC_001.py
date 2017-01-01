c0f37363a7abc3e338d8e3a5c8c4c58f5a3daddf
cyc <cc13ny@users.noreply.github.com>
2017-01-01 13:37:02 -0500

    Simplify LongSubWRC_001.py
    Replace with more-python style codes

--- leetcode/003-Longest-Substring-Without-Repeating-Characters/LongSubWRC_001.py
+++ leetcode/003-Longest-Substring-Without-Repeating-Characters/LongSubWRC_001.py
@@ -4,12 +4,12 @@ class Solution:
     # @return an integer
     def lengthOfLongestSubstring(self, s):
         start, maxlen = 0, 0
-        dict = {s[i]: -1 for i in range(len(s))}
-        for i in range(len(s)):
+        dict = {c: -1 for c in s}
+        for i in xrange(len(s)):
             if dict[s[i]] != -1:
                 while start <= dict[s[i]]:
                     dict[s[start]] = -1
                     start += 1
-            if i - start + 1 > maxlen: maxlen = i - start + 1
+            maxlen = max(maxlen, i - start + 1)
             dict[s[i]] = i
         return maxlen
