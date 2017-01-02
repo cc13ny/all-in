95d7cc0dc5875d93d9a00b58c7b369f100a2a3e6
cyc <cc13ny@users.noreply.github.com>
2017-01-01 23:35:47 -0500

    Create CombSumIV.py
    Pay attention on the consistency of input and output in a recursive algorithm

---
+++ leetcode/377-Combination-Sum-IV/CombSumIV.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        def func(nums, target, combSum):
+            res = 0
+            if target < 0 or target in combSum:
+                return combSum
+            for num in nums:
+                combSum = func(nums, target-num, combSum)
+                res += combSum.get(target-num, 0)
+            combSum[target] = res
+            return combSum 
+        return func(nums, target, {0:1})[target]
