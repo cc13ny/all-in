4889a29cd0ef176f486d347fd6d679fafa5443aa
cyc <cc13ny@users.noreply.github.com>
2017-01-25 22:05:33 -0500

    Create TargetSum.py

---
+++ leetcode/494-Target-Sum/TargetSum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findTargetSumWays(self, nums, S):
+        """
+        :type nums: List[int]
+        :type S: int
+        :rtype: int
+        """
+        res = 0
+        ma = sum(nums)
+        tmp = [0] * (2*ma + 1)
+        cnt = tmp[:]
+        cnt[ma] = 1
+        for num in nums:
+            for i in range(len(cnt)):
+                if cnt[i]:
+                    tmp[i + num] += cnt[i] 
+                    tmp[i - num] += cnt[i] 
+            cnt = tmp[:]
+            tmp = [0] * (2*ma + 1)
+        if -ma <= S <= ma:
+            res = cnt [S + ma]
+        return res
