6b81dd0ba131b4f68fb197cfe534a385cdd858e7
cyc <cc13ny@users.noreply.github.com>
2017-02-09 16:59:22 -0500

    Create WiggleSort.py

---
+++ leetcode/280-Wiggle-Sort/WiggleSort.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def wiggleSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums) - 1):
+            if (i % 2) ^ (nums[i] > nums[i+1]):
+                nums[i], nums[i+1] = nums[i+1], nums[i]
