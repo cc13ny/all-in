dbd9a3cca1572aebed1bbb22979595b628b1ded7
cyc <cc13ny@users.noreply.github.com>
2017-02-16 20:28:02 -0500

    Clean and Simplify Merge2SortedLs_001.py

--- leetcode/021-Merge-Two-Sorted-Lists/Merge2SortedLs_001.py
+++ leetcode/021-Merge-Two-Sorted-Lists/Merge2SortedLs_001.py
@@ -1,35 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
-    # @param {ListNode} l1
-    # @param {ListNode} l2
-    # @return {ListNode}
+class Solution(object):
     def mergeTwoLists(self, l1, l2):
-        
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
         dumpy = ListNode(0)
-        p1 = l1
-        p2 = l2
         p = dumpy
-        
-        if p1 == None:
-            return p2
-        if p2 == None:
-            return p1
-        while p1 != None and p2 != None:
-            if p1.val < p2.val:
-                p.next = p1
-                p1 = p1.next
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                p.next = l1
+                l1 = l1.next
             else:
-                p.next = p2
-                p2 = p2.next
+                p.next = l2
+                l2 = l2.next
             p = p.next
-        if p1 == None:
-            p.next = p2
-        else:
-            p.next = p1
-        
+            
+        p.next = l2 if l2 else l1
         return dumpy.next
