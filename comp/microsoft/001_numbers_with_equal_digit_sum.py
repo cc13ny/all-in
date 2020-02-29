3e1e01c63063801936087b54c0dcc5715e4e9696
Chi Chen <cc2015ex@gmail.com>
2020-02-29 16:19:34 -0500

    [WIP] MCST OA 4 - 14

--- comp/microsoft/001_numbers_with_equal_digit_sum.py
+++ comp/microsoft/001_numbers_with_equal_digit_sum.py
@@ -3,25 +3,19 @@ class Solution:
         # positive and negative numbers
         num = abs(num)
         tot = 0
-        for l in str(num):
-            tot += ord(l) - 48
+        while num:
+            tot += num % 10
+            num //= 10
         return tot
-
     def num_with_equal_digit_sum(self, A):
         res = -1
-        ds_to_top_2 = {}
+        ds_to_top = {}
         for num in A:
             ds = self.digit_sum(num)
-            if ds in ds_to_top_2:
-                arr = ds_to_top_2[ds]
-                i = 0
-                while i < len(arr):
-                    if arr[i] <= num:
-                        break
-                    i += 1
-                arr.insert(i, num)
-                arr = arr[:2]
-                res = max(res, sum(arr))
+            if ds in ds_to_top:
+                top = ds_to_top[ds]
+                res = max(res, top + num)
+                ds_to_top[ds] = max(top, num)
             else:
-                ds_to_top_2[ds] = [num]
+                ds_to_top[ds] = num
         return res
