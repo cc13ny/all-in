c9b40348d92153d2c6fe5d17df130a6c98865fc2
cyc <cc13ny@users.noreply.github.com>
2020-03-24 08:25:36 -0400

    [Leetcode] 1249. Minimum Remove to Make Valid Parentheses

---
+++ leetcode/1249-Minimum-Remove-to-Make-Valid-Parentheses/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        
+        res = ''
+        for c in s:
+            if c == ')':
+                if len(stack) > 0:
+                    stack.pop()
+                    res += c
+            else:
+                res += c
+                if c == '(':
+                    stack.append(len(res)-1)
+           
+        res = list(res)
+        for idx in stack:
+            res[idx] = ''
+            
+        return ''.join(res)
