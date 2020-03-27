12834d57334545520023ba7d3b69bf417f372720
Chi Chen <cc2015ex@gmail.com>
2020-03-26 21:57:57 -0400

    [Leetcode] 680. Valid Palindrome II -- simplify bruce_force_optimized (minor code change, no optimization)

--- leetcode/680-Valid-Palindrome-II/bruce_force_optimized.py
+++ leetcode/680-Valid-Palindrome-II/bruce_force_optimized.py
@@ -1,3 +1,4 @@
+# my original version
 def is_palindrome(st):
     l, r = 0, len(st) - 1
     while l < r:
@@ -20,3 +21,25 @@ class Solution:
             r -= 1
 
         return is_palindrome(s[l:r]) or is_palindrome(s[l + 1:r + 1])
+
+# simplified version
+def is_palindrome(st):
+    l, r = 0, len(st) - 1
+    while l < r:
+        if st[l] != st[r]:
+            return False
+        l += 1
+        r -= 1
+    return True
+
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return is_palindrome(s[l:r]) or is_palindrome(s[l + 1:r + 1])
+            l += 1
+            r -= 1
+
+        return True
