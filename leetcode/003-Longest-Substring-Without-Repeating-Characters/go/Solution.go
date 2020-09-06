29ab9a5ea4f0aefeb27eed91e7c8b97a815a5986
Chi Chen <cc2015ex@gmail.com>
2020-09-06 15:41:05 -0400

    [LEETCODE] Longest Substring Without Repeating Characters (go)

---
+++ leetcode/003-Longest-Substring-Without-Repeating-Characters/go/Solution.go
@@ -0,0 +1,23 @@
+func lengthOfLongestSubstring(s string) int {
+	res, leftmost := 0, 0
+
+	var chars [255]int
+	for i, _ := range chars {
+		chars[i] = -1
+	}
+
+	for i, c := range s {
+		leftmost = max(leftmost, chars[int(c)]+1)
+		res = max(res, i - leftmost + 1)
+		chars[int(c)] = i
+	}
+
+	return res
+}
+
+func max(x, y int) int {
+	if x > y {
+	return x
+	}
+	return y
+}
