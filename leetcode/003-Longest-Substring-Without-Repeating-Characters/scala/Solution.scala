f2ed140969058192965fb07582ae0f393c694174
Chi Chen <cc2015ex@gmail.com>
2020-09-07 10:53:58 -0400

    [LEETCODE] Longest Substring Without Repeating Characters (go)

---
+++ leetcode/003-Longest-Substring-Without-Repeating-Characters/scala/Solution.scala
@@ -0,0 +1,16 @@
+object Solution {
+  def lengthOfLongestSubstring(s: String): Int = {
+    var left_most_idx, tmp_max_len, max_len:Int = 0
+    var chars = Array.fill(255)(-1)
+
+    for((c, i) <- s.view.zipWithIndex) {
+      if (chars(c.toInt) + 1 > left_most_idx) {
+        left_most_idx = chars(c.toInt) + 1
+      }
+      tmp_max_len = i - left_most_idx + 1
+      if (tmp_max_len > max_len) max_len = tmp_max_len
+      chars(c.toInt) = i
+    }
+    return max_len
+  }
+}
